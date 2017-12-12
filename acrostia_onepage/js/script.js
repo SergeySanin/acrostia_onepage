0659b12ec32ffa3793a95a61c9024632b78fed95
sergey.s4nin <sergey.s4nin@gmail.com>
2017-12-12 20:35:42 +0300

    acrostia_onepage

---
+++ acrostia_onepage/js/script.js
@@ -0,0 +1,45 @@
+ $(document).ready(function(){
+    $(".home").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+  $(document).ready(function(){
+    $(".service").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+   $(document).ready(function(){
+    $(".about").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+   $(document).ready(function(){
+    $(".works").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+
+   $(document).ready(function(){
+    $(".contact").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
